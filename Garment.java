5465182f5287e578b88eb9208cb4fa81b90889c1
ifty-13 <ifty2305101641@diu.edu.bd>
2024-11-20 01:32:14 +0600

    All class declared

--- Garment.java
+++ Garment.java
@@ -2,13 +2,30 @@
 public class Garment {
 
     public static void main(String[] args) {
-        
-        
-        
-        
+
         System.out.println("Germany");
-        
+        System.out.println("String");
         System.out.println("Garments");
-        
+
     }
+}
+
+class Fabric {
+
+}
+
+class Supplier {
+
+}
+
+class Customer {
+
+}
+
+class Order {
+
+}
+
+class Inventory {
+
 }
