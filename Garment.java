8bfde010160ba81c2148b3c8c09c6b93f2b561ae
ifty-13 <ifty2305101641@diu.edu.bd>
2024-11-19 23:32:40 +0600

    First Commit

---
+++ Garment.java
@@ -0,0 +1,7 @@
+
+public class Garment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ mini/project/on/garments/MiniProjectOnGarments.java
@@ -0,0 +1,12 @@
+
+package mini.project.on.garments;
+
+
+public class MiniProjectOnGarments {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
