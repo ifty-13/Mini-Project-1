ba70b596bb873087d2408d811fbdf4ea7566c8d1
ifty-13 <ifty2305101641@diu.edu.bd>
2024-11-21 22:56:14 +0600

    New Commit

---
+++ MiniProjectOnGarment.java
@@ -0,0 +1,43 @@
+
+public class MiniProjectOnGarment {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Garment {
+
+    public String id;
+    public String name;
+    public String description;
+    public String size;
+    public String color;
+    public double price;
+    public int stockQuantity;
+
+    void updateStock(int quantity) {
+        this.stockQuantity = quantity;
+    }
+
+}
+
+class Fabric {
+
+}
+
+class Supplier {
+
+}
+
+class Customer {
+
+}
+
+class Order {
+
+}
+
+class Inventory {
+
+}
